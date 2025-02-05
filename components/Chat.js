ca85416ef900302c3b4bc52a357b3509830045f3
GianBo <162351219+Fonchosama@users.noreply.github.com>
2025-02-05 15:16:35 +0100

    task 5.2 finished

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,78 @@
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useState, useEffect } from 'react';
 
-const Chat = ({ route }) => {
-  const { userName, bgColor } = route.params;
+const Chat = ({ route, navigation }) => {
+  const { userName, bgColor, name } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000',
+          },
+          left: {
+            backgroundColor: '#FFF',
+          },
+        }}
+      />
+    );
+  };
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello Developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a system message',
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
+  }, []);
+
+  useEffect(() => {
+    navigation.setOptions({ title: name });
+  }, []);
 
   return (
     <View style={[styles.container, { backgroundColor: bgColor }]}>
       <Text style={styles.header}>Bienvenido al chat, {userName}!</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === 'android' ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
     </View>
   );
 };
@@ -14,13 +80,11 @@ const Chat = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
   },
   header: {
     fontSize: 24,
     fontWeight: 'bold',
+    top: 30,
   },
 });
 
